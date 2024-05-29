09e022aaacbdf67a850f734d6c9006f1ec7c3237
ryan <concepting@protonmail.com>
2024-05-29 12:53:29 -0400

    creating files for learning

--- src/basic python/01.py
+++ src/basic python/01.py
@@ -201,6 +201,43 @@ fruits.remove("banana")
 fruits = {"apple", "banana", "cherry"}
 fruits.discard("banana")
 
+#Use the get method to print the value of the "model" key of the car dictionary.
+car =	{
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car.get("model"))
 
+#Change the "year" value from 1964 to 2020.
+car =	{
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["year"] = 2020
 
+#Add the key/value pair "color" : "red" to the car dictionary.
+car =	{
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["color"] = "red"
+
+#Use the pop method to remove "model" from the car dictionary.
+car =	{
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.pop("model")
+
+#Use the clear method to empty the car dictionary.
+car =	{
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.clear()
 
