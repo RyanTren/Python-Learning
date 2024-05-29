208924c3aac8278b09415d30945349fc9fe56c0b
ryan <concepting@protonmail.com>
2024-05-29 00:36:58 -0400

    python lists and tuples

--- src/basic python/01.py
+++ src/basic python/01.py
@@ -72,5 +72,81 @@ x = int(x)
 x = 5
 x = complex(x)
 
+x = "Hello World"
+print(len(x))
+
+txt = "Hello World"
+x = txt[0]
+
+#Get the characters from index 2 to index 4 (llo).
+txt = "Hello World"
+x = txt[2:5]
+
+#Return the string without any whitespace at the beginning or the end.
+txt = " Hello World "
+x = txt.strip()
+
+#Convert the value of txt to upper case.
+txt = "Hello World"
+x = txt.upper()
+
+#Convert the value of txt to lower case.
+txt = "Hello World"
+x = txt.lower()
+
+#Replace the character H with a J.
+txt = "Hello World"
+x = txt.replace("H", "J")
+
+#Insert the correct syntax to add a placeholder for the age parameter.
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#Use the correct membership operator to check if "apple" is present in the fruits object.
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+#Use the correct comparison operator to check if 5 is not equal to 10.
+if 5 != 10:
+    print("5 and 10 is not equal")
+
+#Use the correct logical operator to check if at least one of two statements is True.
+if 5== 10 or 4 == 4:
+    print("At least one of the statements is true")
+
+#Print the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#Change the value from "apple" to "kiwi", in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+#Use the append method to add "orange" to the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#Use the insert method to add "lemon" as the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#Use the remove method to remove "banana" from the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#Use negative indexing to print the last item in the list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#Use a range of indexes to print the third, fourth, and fifth item in the list.
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#Use the correct syntax to print the number of items in the list.
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
 
 
