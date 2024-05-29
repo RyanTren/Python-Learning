0a869670af2725f447a2058cfc1a23a358fd75f5
ryan <concepting@protonmail.com>
2024-05-29 13:20:06 -0400

    if else done

---
+++ src/basic python/if-else.py
@@ -0,0 +1,62 @@
+#Print "Hello World" if a is greater than b.
+
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#Print "Hello World" if a is not equal to b.
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+#Print "Yes" if a is equal to b, otherwise print "No".
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+#Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+#Print "Hello" if a is equal to b, and c is equal to d.
+a = 50
+b = 50
+c = 10
+d = 10
+if a == b and c == d:
+    print("Hello")
+
+
+#Print "Hello" if a is equal to b, or if c is equal to d.
+if a == b or c == d:
+    print("Hello")
+
+#Complete thje code block, print "YES" if 5 is larger than 2.
+
+if 5 > 2:
+    print("YES")
+
+#Use the correct short hand syntax to put the following statement on one line: if a is greater than b, print "a is greater than b".
+a = 2
+b = 5
+print("YES") if a > b else print("NO")
+
+#Use an if statement to print "YES" if either a or b is equal to c.
+a = 2 
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES")
+
+
