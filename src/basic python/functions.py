b4afc6ac5c7615052e7c924c3b7cbea03fd39b0d
ryan <concepting@protonmail.com>
2024-05-29 13:34:57 -0400

    Update functions.py

--- src/basic python/functions.py
+++ src/basic python/functions.py
@@ -4,3 +4,19 @@ def my_function():
 
 #Execute a function named my_function.
 my_function()
+
+#Inside the function, add a parameter: fname.
+def my_function(fname):
+    print(fname + " Refsnes")
+
+#Let the function return the x parameter + 5.
+def my_function(x):
+    return x + 5
+
+#If you do not know the number of arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix is *.
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
+
+#If you do not know the number of keyword arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix is **.
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
