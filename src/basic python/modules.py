90c81288851923f63e6a15defe833d60a9a2b5d2
ryan <concepting@protonmail.com>
2024-05-29 13:34:25 -0400

    donezo

---
+++ src/basic python/modules.py
@@ -0,0 +1,12 @@
+#What is the correct syntax to import a module named mymodule?
+# import mymodule
+
+#If you want to refer to a module by using a different name, you can create an alias. What is the correct syntax to create an alias for mymodule?
+# import mymodule as mx
+
+#What is the correct syntax to import only the person1 dictionary from the module mymodule?
+# import my module
+#print(dir(mymodule))
+
+#What is the correct syntax to import the platform module?
+# from mymodule import person1
