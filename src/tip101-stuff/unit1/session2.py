e84ed5d337f014ec47aa3fe096469f537c640300
ryan <concepting@protonmail.com>
2024-06-09 21:05:27 -0400

    Update session2.py

--- src/tip101-stuff/unit1/session2.py
+++ src/tip101-stuff/unit1/session2.py
@@ -22,3 +22,68 @@ def print_list(lst):
 lst = ["squirtle", "gengar", "charizard", "pikachu"]
 print_list(lst)
 
+"""
+Problem 2: Print Doubled List
+Write a function doubled() that takes in a list of integers lst as a parameter and prints each item in the list multiplied by two.
+
+def doubled(lst):
+    pass
+Example Input: lst = [1,2,3]
+Example Output:
+
+2
+4
+6
+"""
+
+#Solution to Problem 2
+def doubled(lst):
+  for i in lst:
+    print(i * 2)
+
+lst = [1,2,3]
+doubled(lst)
+
+"""
+Problem 3: Return Doubled List
+Modify the function doubled() so that instead of printing the items, it returns a new list of the doubled numbers.
+
+Example Usage:
+
+lst = [1,2,3]
+new_lst = doubled(lst)
+print(new_lst)
+Example Output:
+
+[2, 4, 6]
+"""
+
+#Problem 3 Solution
+def doubled(lst):
+  new_lst = []
+  for i in range(len(lst)):
+    new_lst.append(lst[i] * 2)
+
+  return new_lst
+
+lst = [1,2,3]
+new_lst = doubled(lst)
+print(new_lst)
+
+"""
+Problem 4: Flip Signs
+Write a function flip_sign() that takes in a list of integers lst as a parameter and returns a new list where each number in the original list has been multiplied by -1.
+
+def flip_sign(lst):
+    pass
+Example Usage:
+
+lst = [1,-2,-3,4]
+flipped_lst = flip_sign(lst)
+print(flipped_lst)
+Example Output:
+
+[-1, 2, 3, -4]
+"""
+
+#Problem 4 Solution
