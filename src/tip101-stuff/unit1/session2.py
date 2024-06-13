b4cb80f5a94458ccce91e3e2dd9101861be25f77
ryan <concepting@protonmail.com>
2024-06-13 13:36:56 -0400

    Update session2.py

--- src/tip101-stuff/unit1/session2.py
+++ src/tip101-stuff/unit1/session2.py
@@ -113,4 +113,16 @@ print(max_diff)  # Output: 20
 
 
 #Problem 6 Solution
+def count_less_than(numbers, threshold):
+  count = 0
+  for i in range(len(numbers)):
+    if numbers[i] < threshold:
+      count += 1
+  return count  
+    
+numbers = [12,8,2,4,4,10]
+counter = count_less_than(numbers,5)
+print(counter) #Output: 3
+
+
 
